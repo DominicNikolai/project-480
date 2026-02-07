a9e43dc13b2121a0b9f5baa60dd872e396bb505e
DominicNikolai <d960960gm@gmail.com>
2026-02-07 15:27:35 +0100

    v0.1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,13 @@ export default defineConfig({
     assets: 'static'
   },
   //Deploy normal project
-  // base: '/project-239/',
-  // site: 'https://dominicnikolai.github.io/project-239/'
+  base: '/project-480/',
+  site: 'https://dominicnikolai.github.io/project-480/'
 
   //Deploy 20 Projects
   // base: '/20-projects-with-css/',
   // site: 'https://20essentials.github.io/20-projects-with-css/'
 
   //Localhost
-  site: 'http://localhost:4321/'
+  // site: 'http://localhost:4321/'
 });
